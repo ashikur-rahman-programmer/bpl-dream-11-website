170047ce2200c548746767074a92195726edd7c0
ashik081 <freelancerashik81@gmail.com>
2026-03-26 21:53:29 +0600

    navbar compled with responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
+import logo from "../assets/logo.png";
+import coin from "../assets/dollar 1.png";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100 shadow-sm">
+    <div className="container mx-auto">
+      <div className="navbar bg-base-100 mt-12 mb-6 shadow-sm flex justify-between items-center">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -28,40 +30,43 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a>Item 1</a>
+                <a>Home</a>
               </li>
               <li>
-                <a>Item 2</a>
+                <a>Fixture</a>
               </li>
               <li>
-                <a>Item 3</a>
+                <a>Teams</a>
               </li>
               <li>
-                <a>Item 3</a>
+                <a>schedules</a>
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">BPL</a>
+          <a className="btn btn-ghost text-xl">
+            <img src={logo} alt="" />
+          </a>
         </div>
-        <div className="navbar-center hidden lg:flex">
+        <div className="navbar-end hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <a>Item 1</a>
+              <a>Home</a>
             </li>
             <li>
-              <a>Item 2</a>
+              <a>Fixture</a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a>Teams</a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a>schedules</a>
             </li>
           </ul>
         </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
-        </div>
+
+        <button className="btn">
+          Coin <img src={coin} alt="" />
+        </button>
       </div>
     </div>
   );
