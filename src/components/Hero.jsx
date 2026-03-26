079b3772a7730dd0b7eab9dbed8788a7b785bfa8
ashik081 <freelancerashik81@gmail.com>
2026-03-26 22:32:48 +0600

    added hero section same as figma design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import bgImage from "../assets/bg-shadow.png";
+import heroImage from "../assets/banner-main.png";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <div className="container mx-auto">
+      {/*  */}
+      <div
+        className="hero "
+        style={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          borderRadius: "16px",
+        }}
+      >
+        <div className="hero-overlay rounded-2xl"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="p-16">
+            <div className="flex justify-center mb-2">
+              <img src={heroImage} alt="hero" />
+            </div>
+            <h1 className="mb-5 text-5xl font-bold">
+              Assemble Your Ultimate Dream 11 Cricket Team
+            </h1>
+            <p className="mb-5 text-2xl text-gray-500 font-semibold">
+              Beyond Boundaries Beyond Limits{" "}
+            </p>
+            <div className="p-1 border border-[#E7FE29] rounded-xl inline-block">
+              <button className="btn bg-[#E7FE29] text-black rounded-xl">
+                Claim Free Credit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
